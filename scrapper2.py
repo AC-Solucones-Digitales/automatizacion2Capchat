6a9cb0fec56c371545b68ef89d98f9711dc7e879
Armando Campos <58012193+armando19993@users.noreply.github.com>
2023-09-01 05:52:09 -0500

    Create scrapper2.py

---
+++ scrapper2.py
@@ -0,0 +1,57 @@
+from selenium_recaptcha_solver import RecaptchaSolver
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+import time
+
+# Base de datos
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="osem.es",
+  user="pym",
+  password="password",
+  database="colombia"
+)
+
+for i in range(1, 10, 1):
+    c=mydb.cursor()
+    c.execute("SELECT * FROM documentos LIMIT 1")
+    result_set = c.fetchall()
+    
+    
+    for row in result_set:
+        options = webdriver.FirefoxOptions()
+        options.headless = True
+        d = webdriver.Firefox(options=options)
+        try:
+            solver = RecaptchaSolver(driver=d)
+            d.get("https://wsp.registraduria.gov.co/censo/consultar")
+            placa = d.find_element(By.ID, 'nuip')
+            placa.send_keys(row[1])
+            recaptcha_iframe = d.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]')
+            solver.click_recaptcha_v2(iframe=recaptcha_iframe)
+            boton=d.find_element(By.NAME, "enviar");
+            d.execute_script("arguments[0].click();", boton)
+            d.implicitly_wait(2)
+            niup=d.find_elements(By.XPATH, '/html/body/div[1]/section[2]/div/div/div[5]/form/div[2]/div[2]/div/table/tbody/tr/td')
+            mycursor = mydb.cursor()
+            if(len(niup) == 6):
+                sql = "INSERT INTO recopilados (niup, departamento, municipio, puesto, direccion, mesa) VALUES (%s, %s, %s, %s, %s, %s)"
+                val = (niup[0].text, niup[1].text, niup[2].text, niup[3].text, niup[4].text, niup[5].text)
+                mycursor.execute(sql, val)
+                mydb.commit()
+                print(mycursor.rowcount, "record inserted.")
+                # cursor2 = mydb.cursor()
+                sql2 = "DELETE FROM documentos WHERE documento = %s"
+                # val2 = (niup[0].text)
+                mycursor.execute(sql2, (niup[0].text,))
+                mydb.commit()
+            time.sleep(20)
+        except:
+            print("el proceso fallo")
+            time.sleep(120)
+            print("Contina la insersion")
+        d.close()
